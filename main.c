cca61010c9729dd19adea805d64435ad1975e45d
Selim Efe <uysalselimefe@gmail.com>
2026-01-14 12:34:38 +0300

    Implement statistical function for variance and standard deviation
    Added 'Istatistik_Hesapla' function using <math.h>. The logic calculates the mean, computes the sum of squared differences, and returns either the variance or its square root based on the input parameter.

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int sapma(int dizi[],int n){
+    int sum=0,i,ort,varyans;
+
+    for(i=0;i<n;i++){
+        sum+=dizi[i];
+    }
+
+    ort=sum/n;
+
+    sum=0;
+
+    for(i=0;i<n;i++){
+        sum+=(dizi[i]-ort)*(dizi[i]-ort);
+    }
+
+    varyans=sqrt(sum/n);
+
+
+
+    return varyans;
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    int n,i;
+
+    n=rand()%5+5;
+
+    int dizi[n];
+
+
+
+    for(i=0;i<n;i++){
+        dizi[i]=rand()%101;
+    }
+
+    for(i=0;i<n;i++){
+        printf("%d ",dizi[i]);
+    }
+
+    printf("\n");
+
+    printf("%d",sapma(dizi,n));
+
+}
